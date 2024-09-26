b56b571cb7c6a392bf6921720502ab45bb0bcea9
Michael Green <michaelgreen06@gmail.com>
2024-09-26 12:01:42 -0600

    finishing up backend work

--- fullstack-ts/server/src/resolvers.ts
+++ fullstack-ts/server/src/resolvers.ts
@@ -1,13 +1,20 @@
 import Query from './resolvers/Query';
+import User from './resolvers/User';
+import Tweet from './resolvers/Tweet';
 import { Resolvers } from './resolvers-types.generated';
-import Db from './db';
+import Db, { DbTweet, DbUser } from './db';
 
 export interface TwitterResolverContext {
   db: Db;
+  dbTweetCache: Record<string, DbTweet>;
+  dbTweetToFavoriteCountMap: Record<string, number>;
+  dbUserCache: Record<string, DbUser>;
 }
 
 const resolvers: Resolvers<TwitterResolverContext> = {
   Query,
+  User,
+  Tweet,
 };
 
 export default resolvers;
